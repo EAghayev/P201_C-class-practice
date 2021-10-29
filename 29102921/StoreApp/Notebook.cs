178188563178d929e655a2b77cfac55cb440af1f
EAghayev <eaghayevv@gmail.com>
2021-10-29 17:04:14 +0400

    project added

---
+++ 29102921/StoreApp/Notebook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreApp
+{
+    class Notebook:Product
+    {
+        Notebook(double price,int count,string model) : base(price, count)
+        {
+            this.Model = model;
+        }
+        Notebook(double price, int count, string model,string brand) : base(price, count)
+        {
+            this.Model = model;
+            this.Brand = brand;
+        }
+        Notebook(double price, int count, string model, string brand,int ram,int storage) : base(price, count)
+        {
+            this.Model = model;
+            this.Brand = brand;
+            this.Ram = ram;
+            this.Storage = storage;
+        }
+
+        public string Brand;
+        public string Model;
+        public int Ram;
+        public int Storage;
+    }
+}
---
+++ 29102921/StoreApp/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreApp
+{
+    class Product
+    {
+        public Product(double price,int count)
+        {
+            this.Price = price;
+            this.Count = count;
+        }
+        public double Price;
+        public int Count;
+    }
+}
